3e898ba693eab0ae1c9881a641472e8282253db0
Cppshizoid <alex.krivoschecov@gmail.com>
2023-11-15 16:31:40 +0700

    Update task.sql

--- 1_semestr/Informatica/9theme/task.sql
+++ 1_semestr/Informatica/9theme/task.sql
@@ -1,8 +1,2 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<SqlQuery>
-<Id>334</Id>
-<Query>
- SELECT title FROM movie
- WHERE director = 'James Cameron'
- </Query>
-</SqlQuery>
+SELECT title FROM movie
+WHERE director = 'James Cameron'
