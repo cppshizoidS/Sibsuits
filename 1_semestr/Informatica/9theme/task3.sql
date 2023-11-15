18a69f2335b253c045b08a2cbc249f49cfe25621
Cppshizoid <alex.krivoschecov@gmail.com>
2023-11-15 16:32:14 +0700

    Update task3.sql

--- 1_semestr/Informatica/9theme/task3.sql
+++ 1_semestr/Informatica/9theme/task3.sql
@@ -1,6 +1,1 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<SqlQuery>
-<Id>452</Id>
- SELECT country, MAX(penalty_count) AS max_penalty_count FROM soccer WHERE penalty_count > 0 GROUP BY country
-  </Query>
-</SqlQuery>
+SELECT country, MAX(penalty_count) AS max_penalty_count FROM soccer WHERE penalty_count > 0 GROUP BY country
