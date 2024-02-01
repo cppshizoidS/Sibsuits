ffdeb086b136bbf42f8babd840af0c67c0eb3187
Cppshizoid <alex.krivoschecov@gmail.com>
2024-02-01 18:19:43 +0700

    Update 1.c

--- 2semester/Programming/6_lab/1.c
+++ 2semester/Programming/6_lab/1.c
@@ -1,6 +1,20 @@
+/**
+ * @file triangle_properties.c
+ * @brief Программа для расчета свойств треугольника: периметра и площади.
+ */
+
 #include <stdio.h>
 #include <math.h>
 
+/**
+ * @brief Рассчитывает свойства треугольника: периметр и площадь.
+ * @param a Длина первой стороны треугольника.
+ * @param b Длина второй стороны треугольника.
+ * @param c Длина третьей стороны треугольника.
+ * @param perimeter Указатель на переменную, в которую будет записан периметр треугольника.
+ * @param area Указатель на переменную, в которую будет записана площадь треугольника.
+ * @return Возвращает 1, если треугольник существует, и 0 в противном случае.
+ */
 int calculateTriangleProperties(double a, double b, double c, double *perimeter, double *area) {
     if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
         return 0; // Треугольник не существует
@@ -14,6 +28,10 @@ int calculateTriangleProperties(double a, double b, double c, double *perimeter,
     return 1; // Треугольник существует
 }
 
+/**
+ * @brief Главная функция программы.
+ * @return Возвращает 0 в случае успешного завершения программы.
+ */
 int main() {
     double side1, side2, side3, perimeter, area;
 
