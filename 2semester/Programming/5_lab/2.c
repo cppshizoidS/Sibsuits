0ceef9a9d884a7c6913d9b68983ea19731593112
Cppshizoid <alex.krivoschecov@gmail.com>
2024-02-01 18:18:09 +0700

    Update 2.c
    added docs

--- 2semester/Programming/5_lab/2.c
+++ 2semester/Programming/5_lab/2.c
@@ -1,7 +1,17 @@
+/**
+ * @file multiplication_table.c
+ * @brief Программа для генерации и вывода таблицы умножения.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-void printMatrix(int **matrix, int size) {
+/**
+ * @brief Вывод матрицы на экран.
+ * @param matrix Матрица для вывода.
+ * @param size Размер матрицы.
+ */
+void printMatrix(int** matrix, int size) {
   for (int i = 0; i < size; i++) {
     for (int j = 0; j <= i; j++) {
       printf("%d\t", matrix[i][j]);
@@ -10,29 +20,34 @@ void printMatrix(int **matrix, int size) {
   }
 }
 
+/**
+ * @brief Главная функция программы.
+ * @return Возвращает 0 в случае успешного завершения программы.
+ */
 int main() {
   int size;
 
   printf("Введите размер таблицы умножения: ");
   scanf("%d", &size);
 
-  int **multiplicationTable = (int **)malloc(size * sizeof(int *));
+  // Выделение памяти под таблицу умножения
+  int** multiplicationTable = (int**)malloc(size * sizeof(int*));
   for (int i = 0; i < size; i++) {
-    multiplicationTable[i] = (int *)malloc((i + 1) * sizeof(int));
+    multiplicationTable[i] = (int*)malloc((i + 1) * sizeof(int));
   }
 
-  // заполнение таблицы умножения
+  // Заполнение таблицы умножения
   for (int i = 0; i < size; i++) {
     for (int j = 0; j <= i; j++) {
       multiplicationTable[i][j] = (i + 1) * (j + 1);
     }
   }
 
-  // вывод таблицы умножения
+  // Вывод таблицы умножения
   printf("Таблица умножения:\n");
   printMatrix(multiplicationTable, size);
 
-  // освобождение памяти
+  // Освобождение памяти
   for (int i = 0; i < size; i++) {
     free(multiplicationTable[i]);
   }
