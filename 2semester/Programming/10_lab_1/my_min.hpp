f11f17333b7d0f01d1a0dc75a205034042d4cebd
Cppshizoid <alex.krivoschecov@gmail.com>
2024-02-14 20:07:40 +0700

    Delete 2semester/Programming/10_lab_1 directory

--- 2semester/Programming/10_lab_1/my_min.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef MY_MIN_HPP
-#define MY_MIN_HPP
-
-template <typename T>
-const T& my_min(const T& a, const T& b) {
-    return (b < a) ? b : a;
-}
-#endif
--- 2semester/Programming/10_lab_1/my_same_as.hpp
+++
@@ -1,5 +0,0 @@
-#include "my_is_same.hpp"
-
-template<typename T, typename U>
-concept SameAs = my_is_same::is_same_v<T,U>;
-
