877d5ce559d6b82349d394afd5087b0843c627a8
Cppshizoid <alex.krivoschecov@gmail.com>
2024-02-15 00:14:20 +0700

    Update main.cpp

--- 2semester/SAOD/2_lab/main.cpp
+++ 2semester/SAOD/2_lab/main.cpp
@@ -1,54 +1,60 @@
 #include "ArrFunc.hpp"
 #include "Sorts.hpp"
 
+#include <iomanip>
 #include <iostream>
 #include <vector>
-#include <iomanip>
 
 int main() {
-    std::cout << "| Size array  | Theoretical bubble               | Theoretical shaker                       | M + C bubble                     | M + C shaker                       | M + C select                       |" << std::endl;
-    std::cout << "|             | decreasing | middle | increasing | decreasing | middle max | increasing max | decreasing | random | increasing | decreasing   | random | increasing | decreasing   | random | increasing |" << std::endl;
-    std::cout << "|-------------|------------|--------|------------|------------|------------|----------------|------------|--------|------------|--------------|--------|------------|--------------|--------|------------|" << std::endl;
+  std::ios::sync_with_stdio(false);
 
-    for (size_t len_arr = 100; len_arr <= 500; len_arr += 100) {
-        std::vector<int> Amin(len_arr);
-        std::vector<int> Amax(len_arr);
-        std::vector<int> Arand(len_arr);
+   std::cout << "| Size array |        Theoretical bubble        |            Theoretical shaker            |           M + C bubble           |           M + C shaker             |           M + C Select         |"<< std::endl;
+   std::cout << "|            | decreasing | middle | increasing | decreasing | middle max | increasing max | decreasing | random | increasing | decreasing   | random | increasing | decreasing | random| increasing|" << std::endl;
+   std::cout << "|------------|------------|--------|------------|------------|------------|----------------|------------|--------|------------|--------------|--------|------------|------------|-------|-----------|" << std::endl;
 
-        FillDec(Amin, len_arr);
-        FillInc(Amax, len_arr);
-        FillRand(Arand, len_arr);
+  for (size_t len_arr = 100; len_arr <= 500; len_arr += 100) {
+    std::vector<int> Amin(len_arr);
+    std::vector<int> Amax(len_arr);
+    std::vector<int> Arand(len_arr);
 
-        std::cout << "| " << std::setw(12) << len_arr;
+    FillDec(Amin, len_arr);
+    FillInc(Amax, len_arr);
+    FillRand(Arand, len_arr);
 
-        std::cout << "| " << std::setw(11) << 4 * (len_arr * len_arr - len_arr) / 2;
-        std::cout << "| " << std::setw(7) << 3 * (len_arr * len_arr - len_arr) / 4 + (len_arr * len_arr - len_arr) / 2;
-        std::cout << "| " << std::setw(11) << (len_arr * len_arr - len_arr) / 2;
+    std::cout << "| " << std::setw(11) << len_arr;
 
-        std::cout << "| " << std::setw(11) << 4 * (len_arr * len_arr - len_arr) / 2;
-        std::cout << "| " << std::setw(11) << 3 * (len_arr * len_arr - len_arr) / 4 + (len_arr * len_arr - len_arr) / 2 - 1;
-        std::cout << "| " << std::setw(15) << (len_arr * len_arr - len_arr) / 2 - 1;
+    std::cout << "| " << std::setw(11) << 4 * (len_arr * len_arr - len_arr) / 2;
+    std::cout << "| " << std::setw(7)
+              << 3 * (len_arr * len_arr - len_arr) / 4 +
+                     (len_arr * len_arr - len_arr) / 2;
+    std::cout << "| " << std::setw(11) << (len_arr * len_arr - len_arr) / 2;
 
-        std::cout << "| " << std::setw(11) << BubbleSort(Amin);
-        std::cout << "| " << std::setw(7) << BubbleSort(Arand);
-        std::cout << "| " << std::setw(11) << BubbleSort(Amax);
+    std::cout << "| " << std::setw(11) << 4 * (len_arr * len_arr - len_arr) / 2;
+    std::cout << "| " << std::setw(11)
+              << 3 * (len_arr * len_arr - len_arr) / 4 +
+                     (len_arr * len_arr - len_arr) / 2 - 1;
+    std::cout << "| " << std::setw(15) << (len_arr * len_arr - len_arr) / 2 - 1;
 
-        FillDec(Amin, len_arr);
-        FillInc(Amax, len_arr);
-        FillRand(Arand, len_arr);
+    std::cout << "| " << std::setw(11) << BubbleSort(Amin);
+    std::cout << "| " << std::setw(7) << BubbleSort(Arand);
+    std::cout << "| " << std::setw(11) << BubbleSort(Amax);
 
-        std::cout << "| " << std::setw(13) << ShakerSort(Amin);
-        std::cout << "| " << std::setw(7) << ShakerSort(Arand);
-        std::cout << "| " << std::setw(11) << ShakerSort(Amax);
+    FillDec(Amin, len_arr);
+    FillInc(Amax, len_arr);
+    FillRand(Arand, len_arr);
 
-        FillDec(Amin, len_arr);
-        FillInc(Amax, len_arr);
-        FillRand(Arand, len_arr);
+    std::cout << "| " << std::setw(13) << ShakerSort(Amin);
+    std::cout << "| " << std::setw(7) << ShakerSort(Arand);
+    std::cout << "| " << std::setw(11) << ShakerSort(Amax);
 
-        std::cout << "| " << std::setw(13) << SelectSort(Amin);
-        std::cout << "| " << std::setw(7) << SelectSort(Arand);
-        std::cout << "| " << std::setw(10) << SelectSort(Amax) << " |" << std::endl;
-    }
+    FillDec(Amin, len_arr);
+    FillInc(Amax, len_arr);
+    FillRand(Arand, len_arr);
 
-    return 0;
+    std::cout << "| " << std::setw(11) << SelectSort(Amin);
+    std::cout << "| " << std::setw(6) << SelectSort(Arand);
+    std::cout << "| " << std::setw(9) << SelectSort(Amax) << " |" << std::endl;
+  }
+
+  return 0;
 }
