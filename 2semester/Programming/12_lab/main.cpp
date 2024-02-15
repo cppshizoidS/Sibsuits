cb7bcca3412c717a954ef07b62ee83bc73ec855f
Cppshizoid <alex.krivoschecov@gmail.com>
2024-02-15 10:35:05 +0700

    Update main.cpp

--- 2semester/Programming/12_lab/main.cpp
+++ 2semester/Programming/12_lab/main.cpp
@@ -2,9 +2,11 @@
 #include <vector>
 
 auto main() -> int {
-  std::vector<int> myArray = {5, 2, 9, 1, 6, 5};
+  std::vector<int> myArray;
 
-  Sorting::measureTime(myArray, Sorting::bubbleSort, "bubble sort");
-  Sorting::measureTime(myArray, Sorting::selectionSort, "selection sort");
-  Sorting::measureTime(myArray, Sorting::insertionSort, "insertion sort");
+  int arraySize = 10000;
+
+  Sorting::measureTime(myArray, Sorting::bubbleSort, "bubble sort", arraySize);
+  Sorting::measureTime(myArray, Sorting::selectionSort, "selection sort", arraySize);
+  Sorting::measureTime(myArray, Sorting::insertionSort, "insertion sort", arraySize);
 }
