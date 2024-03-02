a885979638dfb5d4eb69b5367d2d4e6a5049a234
cppshizoid <alex.krivoschecov@gmail.com>
2024-03-02 22:43:02 +0700

    5th lab

---
+++ 2semester/SAOD/5_lab/Sorts.hpp
@@ -0,0 +1,160 @@
+#ifndef SORTS_HPP
+#define SORTS_HPP
+
+#include "ArrFunc.hpp"
+
+#include <algorithm>
+#include <cstddef>
+#include <iostream>
+/**
+ * @brief Swap the values of two variables.
+ * @tparam T Type of the variables.
+ * @param x First variable.
+ * @param y Second variable.
+ */
+template <typename T> void Swap(T &x, T &y) {
+  T temp = x;
+  x = y;
+  y = temp;
+}
+/**
+ * @brief Print the sorted array along with the number of moves and comparisons.
+ * @tparam T Type of the array elements.
+ * @param A The array to be printed.
+ * @param m Number of moves performed during sorting.
+ * @param c Number of comparisons performed during sorting.
+ */
+template <typename T> void PrintArr(const T &A, size_t m, size_t c) {
+  std::cout << "Sort array: ";
+  PrintMas(A);
+  std::cout << "Move: " << m << std::endl;
+  std::cout << "Compare: " << c << std::endl;
+}
+/**
+ * 1 lab
+ * @brief Perform selection sort on the array.
+ * @tparam T Type of the array elements.
+ * @param A The array to be sorted.
+ * @return Total number of moves and comparisons during sorting.
+ */
+template <typename T> size_t SelectSort(T &A) {
+  size_t m = 0, c = 0;
+  size_t len = A.size();
+  for (size_t i = 0; i < len - 1; i++) {
+    auto minIndex = std::min_element(A.begin() + i, A.end()) - A.begin();
+    Swap(A[i], A[minIndex]);
+    m += 3;
+    c += len - i - 1;
+  }
+  // PrintArr(A, m, c);
+  return m + c;
+}
+/**
+ * 2 lab
+ * @brief Perform bubble sort on the array.
+ * @param T Type of the array elements.
+ * @param A The array to be sorted.
+ * @return Total number of moves and comparisons during sorting.
+ */
+template <typename T> size_t BubbleSort(T &A) {
+  size_t m = 0, c = 0;
+  size_t len = A.size();
+  for (size_t i = 0; i < len; i++) {
+    for (size_t j = 1; j < len - i; j++) {
+      if (A[j] < A[j - 1]) {
+        Swap(A[j], A[j - 1]);
+        m += 3;
+      }
+      c++;
+    }
+  }
+  // PrintArr(A, m, c);
+  return m + c;
+}
+/**
+ * 3 lab
+ * @brief Perform shaker sort (or cocktail sort) on the array.
+ * @tparam T Type of the array elements.
+ * @param A The array to be sorted.
+ * @return Total number of moves and comparisons during sorting.
+ */
+template <typename T> size_t ShakerSort(T &A) {
+  size_t L = 0, R = A.size() - 1, k = A.size() - 1;
+  size_t m = 0, c = 0;
+  do {
+    for (size_t j = R; j > L; j--) {
+      if (A[j] < A[j - 1]) {
+        Swap(A[j], A[j - 1]);
+        m += 3;
+        k = j;
+      }
+      c++;
+    }
+    L = k;
+    for (size_t j = L; j < R; j++) {
+      if (A[j] > A[j + 1]) {
+        Swap(A[j], A[j + 1]);
+        m += 3;
+        k = j;
+      }
+      c++;
+    }
+    R = k;
+  } while (L < R);
+  // PrintArr(A, m, c);
+  return m + c;
+}
+/**
+ * 4 lab
+ * @brief Perform insertion sort on the array.
+ * @tparam T Type of the array elements.
+ * @param A The array to be sorted.
+ * @return Total number of moves and comparisons during sorting.
+ */
+template <typename T> size_t InsertionSort(T &A, size_t len) {
+  size_t m = 0, c = 0;
+  for (size_t i = 1; i < len; i++) {
+    bool f = true;
+    auto t = A[i];
+    long long j = i - 1;
+    while (j > -1 and t < A[j]) {
+      f = false;
+      A[j + 1] = A[j];
+      j--;
+      c++;
+      m++;
+    }
+    if (f)
+      c++;
+    A[j + 1] = t;
+    m += 2;
+  }
+  return m + c;
+}
+/**
+ * @brief Perform shell sort on the array
+ * 
+ * @tparam T Type of the array elements.
+ * @param A The array to be sorted.
+ * @return Total number of moves and comparisons during sorting.
+ */
+template <typename T> size_t ShellSort(T &A) {
+  size_t m = 0, c = 0;
+  size_t len = A.size();
+  for (size_t gap = len / 2; gap > 0; gap /= 2) {
+    for (size_t i = gap; i < len; i++) {
+      auto temp = A[i];
+      size_t j;
+      for (j = i; j >= gap && A[j - gap] > temp; j -= gap) {
+        A[j] = A[j - gap];
+        m += 3;
+        c++;
+      }
+      A[j] = temp;
+      m += 2;
+    }
+  }
+  // PrintArr(A, m, c);
+  return m + c;
+}
+#endif
---
+++ 2semester/SAOD/5_lab/main.cpp
@@ -0,0 +1,38 @@
+#include "ArrFunc.hpp"
+#include "Sorts.hpp"
+
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::cout << "| Size array |        Theoretical shell         |           M + C shell            |\n";
+    std::cout << "|            | decreasing | middle | increasing | decreasing | random | increasing |\n";
+    std::cout << "|------------|------------|--------|------------|------------|--------|------------|\n";
+
+    for (size_t len_arr = 100; len_arr <= 500; len_arr += 100) {
+        std::vector<int> Amin(len_arr);
+        std::vector<int> Amax(len_arr);
+        std::vector<int> Arand(len_arr);
+
+        FillDec(Amin, len_arr);
+        FillInc(Amax, len_arr);
+        FillRand(Arand, len_arr);
+
+        std::cout << "| " << std::setw(11) << len_arr;
+
+        std::cout << "| " << std::setw(11) << ((len_arr * len_arr - len_arr) + 2 * len_arr - 2);
+        std::cout << "| " << std::setw(7) << ((len_arr * len_arr - len_arr) + 5 * (len_arr - 1)) / 2;
+        std::cout << "| " << std::setw(11) << 3 * (len_arr - 1);
+
+        std::cout << "| " << std::setw(11) << ShellSort(Amin);
+        std::cout << "| " << std::setw(7) << ShellSort(Arand );
+        std::cout << "| " << std::setw(11) << ShellSort(Amax) << "|\n";
+
+        FillDec(Amin, len_arr);
+        FillInc(Amax, len_arr);
+        FillRand(Arand, len_arr);
+    }
+
+    return 0;
+}
