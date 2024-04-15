10818d3c622fad4dc193841ee993d348c40f2479
cppshizoid <alex.krivoschecov@gmail.com>
2024-04-15 15:51:23 +0700

    change std::cout to std::print

--- 2semester/SAOD/9_lab/main.cpp
+++ 2semester/SAOD/9_lab/main.cpp
@@ -1,27 +1,27 @@
 #include <iomanip>
-#include <iostream>
+#include <print>
 #include <vector>
+#include <cmath>
 
-#include "ArrFunc.hpp"
+#include "BaseArrFunc.hpp"
 #include "Sorts.hpp"
 
-int main() {
-    std::cout << "|  N  | M + C teoretic |          M + C factic          |" << std::endl;
-    std::cout << "|     |                | decriment | random | increment |" << std::endl;
-    std::cout << "|-----|----------------|-----------|--------|-----------|" << std::endl;
+auto main() -> int {
+    std::println("|  N  | M + C teoretic |          M + C factic          |");
+    std::println("|     |                | decriment | random | increment |");
+    std::println("|-----|----------------|-----------|--------|-----------|");
 
     for (size_t len = 100; len <= 500; len += 100) {
         std::vector<int> arr(len);
 
-        std::cout << "|" << std::setw(5) << len;
-        std::cout << "|" << std::setw(16) << static_cast<int>(2 * len * std::log2(len) + len + 2 + len * std::log2(len) + 6.5 * len - 4);
+        std::print("|{:5}|{:16}|", len, static_cast<int>(2 * len * std::log2(len) + len + 2 + len * std::log2(len) + 6.5 * len - 4));
 
         FillDec(arr, len);
-        std::cout << "|" << std::setw(11) << HeapSort(arr, len);
+        std::print("{:^11}|", HeapSort(arr, len));
         FillRand(arr, len);
-        std::cout << "|" << std::setw(8) << HeapSort(arr, len);
+        std::print("{:^8}|", HeapSort(arr, len));
         FillInc(arr, len);
-        std::cout << "|" << std::setw(10) << HeapSort(arr, len) << " |" << std::endl;
+        std::println("{:^11}|", HeapSort(arr, len));
     }
 
     return 0;
